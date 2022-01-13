d4eff7324f03bb815cd057f873135f65f7219504
SandiS2703 <sandis2703@gmail.com>
2022-01-13 21:30:20 +0700

    Update fitur jadwal dokter(Yaris)

--- src/DAO/JadwalDokterDAO.java
+++ src/DAO/JadwalDokterDAO.java
@@ -5,7 +5,6 @@ import java.util.List;
 import pojo.JadwalDokter;
 
 public interface JadwalDokterDAO {
-	public void getJadwalbyId_Dokter(int id_dokter);
 	public List<JadwalDokter> getAllJadwalDokter();
 	public void createJadwal(JadwalDokter jadwal);
 	public void readJadwal(JadwalDokter jadwal);
